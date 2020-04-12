aa8180749cc48877ac12e57c14e8171d08db3eab
jhgu <jhgu.dev@gmail.com>
2020-04-12 19:31:06 +0900

    Complete Drawing Paths and Shapes

---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/RotateBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotateBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by GJH on 2020/04/12.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotateBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotateBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotateBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
